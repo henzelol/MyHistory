9aeaa72c17ec76b26ec517a4a01654b1d4da4fee
Henzel Santos <henzelmsn3002@hotmail.com>
2017-10-10 10:55:43 -0300

    Add files via upload

---
+++ alo.py
@@ -0,0 +1,22 @@
+a=int(input('Voce tem certeza que voce quer conhecer minha historia, é bem dahora\n (1=Sim,2=Nao,3=Sair)\n'))
+
+while a!=3 :
+
+	if a == 1 :
+		print('Voce nao sabe ainda do que é,mas tudo bem, vou te contar')
+	elif a ==2 :
+		print('nao me importo com sua opiniao, vou contar mesmo assim
+	elif a == 3 :
+		print('ridiculo')
+	
+b=str(input('vamos começar.primeiro vamos voltar lá pra 2012,Minha oitava série, laaaa em julho da oitava serie.Gostou de lá')
+
+if b == 'sim' or b =='SIM' or b 'S' or b =='s' :
+	print('Pois é meu bom, eu tambem.OH ano louco viu')
+if b =='NAO' or b =='nao' orb == 'n' or b == 'N' :
+	print('que isso rapaz,a vida é linda e a oitava série tambem.Nao se preocupar com as notas é muito bom.')
+
+
+
+	
+			
